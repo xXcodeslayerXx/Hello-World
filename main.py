4a814570ecc90bd245e952cd88de238d3aa97b31
xXcodeslayerXx <rileynewman24@gmail.com>
2024-03-20 12:40:40 +1100

    I made a loop changing the amount of o's in the print "Hello World!" each time randomly

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import random
 print("Hello World!")
+
+ans = input("Print again? ")
+
+while ans != "":
+    oNum = random.randint(1,100)
+    print(f"Hell{oNum * 'o'} World!")
+    ans = input("Print again? ")
+
+
